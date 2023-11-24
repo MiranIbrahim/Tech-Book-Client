beeb2694e57d7c671bf492c746f7cab39c712224
Miran Ibrahim <moeenuddin.miran@gmail.com>
2023-11-24 12:23:37 +0600

    read me file added

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+
+
+const Navbar = () => {
+    return (
+        <div>
+            eta kintu navbar
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            Hay hay home,
+        </div>
+    );
+};
+
+export default Home;
